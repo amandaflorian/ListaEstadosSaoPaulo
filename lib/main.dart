06d8b6213203dede0f1c184f514053891f63af99
amandaflorian <a.florian@outlook.com.br>
2021-10-22 13:52:35 -0300

    Exibicao palavras ao atualizar

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,78 @@
-import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
-import 'Dart:async' show Future;
-import 'package:flutter/services.Dart' show rootBundle;
+import 'package:flutter/material.dart';
 
 void main() => runApp(listestado());
 
 class listestado extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
     return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Welcome to Flutter'),
-        ),
-        body: Center(
-          child: Text(wordPair.asPascalCase),
-        ),
-      ),
+      title: 'Startup Name Generator',
+      home: RandomWords(),
     );
   }
 }
 
-// Future<String> loadAsset(BuildContext context) async {
-//   return await DefaultAssetBundle.of(context).loadString('assets/cidades.txt');
-// }
+class RandomWords extends StatefulWidget {
+  @override
+  _RandomWordsState createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+    final _suggestions = <WordPair>[];
+    final _biggerFont = const TextStyle(fontSize: 18.0);
+
+    Widget _buildRow(WordPair pair) {
+      return ListTile(
+        title: Text(
+          pair.asPascalCase,
+          style: _biggerFont,
+        ),
+      );
+    }
+
+    Widget _buildSuggestions() {
+      return ListView.builder(
+          padding: const EdgeInsets.all(16.0),
+          itemBuilder: (context, i) {
+            if (i.isOdd) return const Divider();
+
+            final index = i ~/ 2;
+            if (index >= _suggestions.length) {
+              _suggestions.addAll(generateWordPairs().take(10));
+            }
+            return _buildRow(_suggestions[index]);
+          });
+    }
+
+    return Text(wordPair.asPascalCase);
+  }
+}
+
+class InfiniteList extends RandomWords {
+  Widget build(BuildContext context) {
+    return FutureBuilder<String>(
+      future: loadString('assets/cidades.txt'),
+      builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(child: Text('Please wait'));
+        } else {
+          if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } else
+            return Center(child: new Text('${snapshot.data}'));
+        }
+      },
+    );
+  }
+
+  Future<String> loadAsset(BuildContext context) async {
+    return await DefaultAssetBundle.of(context)
+        .loadString('assets/cidades.txt');
+  }
+
+  loadString(String s) {}
+}
